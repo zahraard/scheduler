fe356698aefe4f52e6467a9c46108b3ba2dc6ed1
Zahra Vazifeh Ardalani <zahra.ardalani@gmail.com>
2020-10-02 11:11:25 -0400

    Add UseVisualMode Hook and test

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+
+function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode)
+  const [history, setHistory] = useState([initialMode]);
+
+  const transition = (newMode, replacing = false)=>{
+    setMode(newMode);
+    if(replacing){
+      const newHistory = pop();
+      setHistory(newHistory.concat(newMode))
+    } else {
+      setHistory([...history, newMode])
+    }
+  }
+  const pop = () => {
+    return history.slice(0, history.length -1);
+  }
+  const back = ()=>{
+    if(history.length > 1){
+      const newHistory = pop();
+      setMode(newHistory[newHistory.length-1])
+      setHistory(newHistory)
+    }    
+  }
+  return {
+    mode,
+    transition,
+    back
+  }
+
+}
+
+export default useVisualMode;
